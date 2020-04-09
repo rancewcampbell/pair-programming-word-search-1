520fb01e4a740e8af03a94f63d821505db6037dd
Rance Campbell <rancewcampbell@gmail.com>
2020-04-09 01:18:38 +0000

    finished word-search w/ Calvin. Function works forward and backward

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,37 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalReverse = letters.map(el => el.reverse().join(''));
+  const verticalReverse = transpose(letters).map(el => el.reverse().join(''));
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let l of horizontalReverse) {
+    if (l.includes(word)) return true;
+  }
+  for (let l of verticalReverse) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+  const newMatrix = [];
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (newMatrix[col]) {
+        newMatrix[col].push(matrix[row][col]);
+      } else {
+        newMatrix.push([matrix[row][col]]);
+      }
+    }
+  }
+  return newMatrix;
+};
+
+
+
+module.exports = wordSearch;
